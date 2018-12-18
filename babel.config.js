ec4c42c6a54da3a6627cedbda0a95b362e97144e
Victor Santos <Victor.Santos@ipsoft.com>
2018-12-17 19:09:14 -0500

    Fixing Babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,31 +1,28 @@
-module.exports = api => {
-  api.cache(true);
-  return {
-    presets: [
-      [
-        "@babel/preset-env",
-        {
-          loose: true,
-          modules: false,
-          useBuiltIns: "entry",
-          forceAllTransforms: api.env("production"),
-          targets: {
-            browsers: ["last 2 versions"]
-          }
+module.exports = api => ({
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        loose: true,
+        modules: false,
+        useBuiltIns: "entry",
+        forceAllTransforms: api.env("production"),
+        targets: {
+          browsers: ["last 2 versions"]
         }
-      ],
-      "@babel/preset-react"
+      }
     ],
-    plugins: [
-      ["@babel/plugin-proposal-class-properties", { loose: true }],
-      "@babel/plugin-transform-classes",
-      "@babel/plugin-proposal-object-rest-spread",
-      "@babel/plugin-transform-async-to-generator",
-      "@babel/plugin-syntax-dynamic-import",
-      "react-hot-loader/babel",
-      "@babel/plugin-transform-modules-commonjs",
-      "@babel/plugin-proposal-export-namespace-from",
-      "@babel/plugin-proposal-throw-expressions"
-    ]
-  };
-};
+    "@babel/preset-react"
+  ],
+  plugins: [
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    "@babel/plugin-transform-classes",
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-transform-async-to-generator",
+    "@babel/plugin-syntax-dynamic-import",
+    "react-hot-loader/babel",
+    "@babel/plugin-transform-modules-commonjs",
+    "@babel/plugin-proposal-export-namespace-from",
+    "@babel/plugin-proposal-throw-expressions"
+  ]
+});
