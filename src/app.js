f5cba5cb3660832621e4009de59ca4d8fc70dab1
Victor Santos <Victor.Santos@ipsoft.com>
2018-12-20 12:19:43 -0500

    React alpha installed and testing hooks

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { hot } from "react-hot-loader";
 
-const App = () => <div>Initial App</div>;
-
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h2> Initial App </h2>
+      <p>Count is {count}</p>
+      <button onClick={() => setCount(count + 1)}>Press Me</button>
+    </div>
+  );
+};
 export default hot(module)(App);
